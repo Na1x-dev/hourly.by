c646174458165fc7da5bad9b3c00b8559235c6f8
Andei <na1x.1024@gmail.com>
2024-06-24 16:49:56 +0300

    feat: connect db

--- hourly/hourly/wsgi.py
+++ hourly/hourly/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hourly-back project.
+WSGI config for hourly project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hourly-back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hourly.settings')
 
 application = get_wsgi_application()
