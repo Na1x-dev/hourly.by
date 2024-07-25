5addebb62b3a610e1af5b06e123ac2acb88221ac
Andei <na1x.1024@gmail.com>
2024-07-26 01:48:10 +0300

    feat: normalized styles of cards

--- hourly-front/src/jsx/Apartment.jsx
+++ hourly-front/src/jsx/Apartment.jsx
@@ -3,19 +3,23 @@ const Apartment = (apartment) => {
 
     return (
         <div className='apartment-card'>
-            <img
+            <img className="card-image"
                 src={apartment.apartment.image_url} // Предполагаем, что у вас есть поле imageUrl
                 // alt={apartment.apartment.name}
                 // onError={(e) => { e.target.src = placeholderImage; }} // Замена на запасное изображение
                 style={{ width: '100%', height: 'auto' }} // Настройте стили по вашему усмотрению
             />
             <p className="apartment-card-text apartment-title">{apartment.apartment.title}</p>
-            <p className="apartment-card-text apartment-room-type">{apartment.apartment.room_type}</p>
-            <p className="apartment-card-text apartment-location">{apartment.apartment.location}</p>
-            <p className="apartment-card-text apartment-price-per-day">{apartment.apartment.price_per_day}</p>
-            <p className="apartment-card-text apartment-rating">{apartment.apartment.rating}</p>
-            <p className="apartment-card-text apartment-size">{apartment.apartment.size}</p>
-
+            <div className="card-text">
+                <div className="horizontal-line"></div>
+                <p className="apartment-card-text apartment-room-type">{apartment.apartment.room_type}</p>
+                <p className="apartment-card-text apartment-location">address: {apartment.apartment.location}</p>
+                <p className="apartment-card-text apartment-price-per-day">{apartment.apartment.price_per_day} per day</p>
+                <div className="horizontal-line"></div>
+                <p className="apartment-card-text apartment-rating">{apartment.apartment.rating}/10</p>
+                <p className="apartment-card-text apartment-size">{apartment.apartment.size}</p>
+            </div>
+            <button className="btn btn-primary see-card-button">See</button>
         </div>
 
     );
