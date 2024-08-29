75d6232848de802f90816befd3d39b18a9e431e2
Na1x-dev <na1x.1024@gmail.com>
2024-08-29 16:40:56 +0300

    feat: add errors when picked least 2 days and notifications

--- hourly-front/src/jsx/BookingCalender.jsx
+++ hourly-front/src/jsx/BookingCalender.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
+import { ToastContainer, toast } from 'react-toastify';
+import '../style/Notify.css'
 import 'react-calendar/dist/Calendar.css';
 import '../style/SearchStyles.css'
+
 
 const BookingCalendar = ({ dateRange, setDateRange }) => {
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
@@ -9,12 +12,31 @@ const BookingCalendar = ({ dateRange, setDateRange }) => {
   const titleDisabled = ({ date }) => {
     return date < new Date();
   }
+  const selectError = () => {
+    toast.dismiss();
+    toast.error('Please select at least 2 days.', {
+      autoClose: 3000,
+      onClick: () => {
+        toast.dismiss(); 
+      },
+    });
+  };
 
   const handleDateChange = (dates) => {
+    if (!dates || dates.length !== 2) {
+      selectError();
+      return;
+    }
+
+    const [start, end] = dates;
+    if (end - start < 2 * 24 * 60 * 60 * 1000 - 1) {
+      selectError();
+      return;
+    }
+
     setDateRange(dates);
     setIsCalendarOpen(false); // Закрыть календарь после выбора дат
   };
-
 
   const toggleCalendar = () => {
     setIsCalendarOpen(!isCalendarOpen);
@@ -22,9 +44,11 @@ const BookingCalendar = ({ dateRange, setDateRange }) => {
 
   return (
     <div className="booking-calendar">
+      
       <div className="input font" onClick={toggleCalendar}>
         {dateRange[0].toLocaleDateString()} - {dateRange[1].toLocaleDateString()}
       </div>
+      <ToastContainer/>
       {isCalendarOpen && (
         <div className="calendar-container font">
           <Calendar
@@ -35,6 +59,7 @@ const BookingCalendar = ({ dateRange, setDateRange }) => {
           />
         </div>
       )}
+      
     </div>
   );
 };
